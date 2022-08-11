02a26db947c53f23dac07d761ab6de559a51586c
Joël Perras <joel@nerderati.com>
2022-08-11 18:55:19 -0400

    Add tests/ to sys.path for easier pytest runs

--- tests/test_flask_boto3.py
+++ tests/test_flask_boto3.py
@@ -1,10 +1,16 @@
-import os
+import os, sys
+
+# A bit of a hack, but let's add the current directory where this conftest is to
+# sys.path, so that we can run `pytest` directly here without having to make the
+# current project editable (`pip install -e`) or without having to invoke it
+# with `python -m pytest` (which effectively does the sys.path add).
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
 
 from unittest import TestCase
 from mock import patch
 
-from flask import Flask
-from flask import _app_ctx_stack as stack
+from flask import Flask, g
 from flask_boto3 import Boto3
 
 
